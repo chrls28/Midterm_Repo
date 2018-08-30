b8f51a0cf329bb9e8ebad9d7fd894ce564d85a4a
chrls28 <cdyf.28@gmail.com>
2018-08-30 16:00:19 +0800

    CDYFSW6AUG302018

---
+++ Seatwork 6 Midterm/SW6.R
@@ -0,0 +1,78 @@
+library(imager)
+library(argparse)
+
+
+
+#DISPLAY IMAGE IMAGER
+glitch <- load.image("img/glitch.jpg") %>% plot
+
+abstract <- load.image("img/abstract.jpg") %>% plot
+
+spiral <- load.image("img/spiral.jpg") %>% plot
+
+#RESIZE IMAGER
+#library = imager resize image local
+resizeimg <- function(img,height,width){
+imgrsze <- resize(img, size_x = height, size_y = width)
+save.image(im = imgrsze,"img/glitchresize.jpg")
+plot(imgrsze)
+}
+
+resizeimg(glitch,250,250)
+
+#GRAYSCALE IMAGER
+#library = imager grayimg local
+grayimg <- function(img,x){
+  is.logical(x)
+if(x==TRUE){
+  gray <- grayscale(img)
+  gray1 <- save.image(gray,"img/spiralgray.jpg")
+  plot(gray)
+} else{
+  plot(img)
+}
+}
+#grayimg local
+grayimg(spiral,TRUE)
+grayimg(spiral,FALSE)
+
+
+#INVERTION IMAGER
+#library = imager invertion of img local
+invertimg <- function(img,angle){
+  invimg <- imrotate(img,angle)
+  invimg1 <- save.image(invimg,"img/abstractinvert.jpg")
+  plot(invimg)
+}
+invertimg(abstract,180)
+
+# FUNTCTION FOR RESISZING INVERTING AND GRAYSCALING
+augmented <- function(img,height,width,x,angle){
+  if(x==TRUE)
+  {
+    is.logical(x)
+    if(x==TRUE){
+      gray <- grayscale(img)
+      gray1 <- save.image(gray,"img/spiralgray.jpg")
+      plot(gray)
+    } else{
+      plot(img)
+    }
+  }
+  if(height&&width > 1){
+    imgrsze <- resize(img, size_x = height, size_y = width)
+    save.image(im = imgrsze,"img/glitchresize.jpg")
+    plot(imgrsze)
+  }
+  if(angle>1){
+    invimg <- imrotate(img,angle)
+    invimg1 <- save.image(invimg,"img/abstractinvert.jpg")
+    plot(invimg)
+  }
+  else{
+    plot(img)
+  }
+}
+
+augmented(abstract,25,25,TRUE,50)
+
---
+++ Seatwork 6 Midterm/Test.R
@@ -0,0 +1,2 @@
+library("argparse")
+example("ArgumentParser")
