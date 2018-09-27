1ab83452d128cffd5373be94c1197630130d37f5
chrls28 <cdyf.28@gmail.com>
2018-09-27 10:52:05 +0800

    new

--- Exam Midterm/MidExam.R
+++ Exam Midterm/MidExam.R
@@ -1,18 +1,19 @@
 library (argparse)
 library (imager)
 library (stringr)
-
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~PARSING COMMAND LINE ARGUMENTS
 parser <- ArgumentParser()
-parser$add_argument("-image",required="True" ,help = "Local File Directory of image to augment :Reqd = TRUE")
+parser$add_argument("-folder",required="True" ,help = "Local File Directory of images to augment :Reqd = TRUE")
 parser$add_argument("-resize",dest="HxW",help = "To resize an image :Default = 0")
 parser$add_argument("-angle",default=0,type="integer", help = "To change the angle of the image :Default = 0")
 parser$add_argument("-grey" ,default=FALSE,help = "To grayscale an image :Default = FALSE")
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~FUNCTION FOR SHOWING HELP/USAGE OF PROGRAM
 cat(parser$print_help(), "\n")
 
 args <- parser$parse_args()
 
-#splitting Height and Width 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~CODE FOR RESIZE INPUT OF HxW
 num <- args$HxW
 HxW <- str_split(num, "x", simplify = TRUE)
 H1 <- as.numeric(HxW[1])
@@ -20,40 +21,53 @@ W1 <- as.numeric(HxW[2])
 H1[is.na(H1)] <- 0
 W1[is.na(W1)] <- 0
 
-#printing desired outputs in string
-cat("Current working dir/image: ", args$image, "\n")
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~PRINTING OUTPUT OF USER IN TXT
+cat("Current working dir/images: ", args$folder, "\n")
 cat("Current input Height: ", H1, "\n")
 cat("Current input Width: ", W1, "\n")
 cat("Current input for grayscale: ", args$grey, "\n")
 cat("Current input angle: ", args$angle, "\n")
 
-#creating another file to store augmented images
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~FUNCTION FOR LOADING THE IMAGE FILES WITH EXTENSION JPG
+getimage<- function(x){
+  y <- list.files(path=x,pattern = "\\.jpg$", ignore.case=TRUE, recursive = TRUE,full.names = TRUE) 
+  lapply(y, load.image)
+} 
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~FUNCTION FOR GETTING THE BASE NAME OF IMAGE FILES
+getname<- function(x){
+  y <- list.files(path=x,pattern = "\\.jpg$", ignore.case=TRUE, recursive = TRUE,full.names = TRUE) 
+  lapply(y,basename)
+} 
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~AUGMENTED IMAGE FILE STORAGE
 dir.create(file.path("Augmented_images"), showWarnings = FALSE)
 
-fileimge <-function(name){
-  file_path_sans_ext(name)
-}
-#augmenting image function
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~AUGMENATION FUNCTION GRAYSCALE:RESIZE:ROTATE
 augmented <- function(img,H,W,gry,angle){
-  imge <- load.image(img)
-  if(gry==TRUE)
-    {
-      is.logical(gry)
-      if(gry==TRUE){
-        gray <- grayscale(imge)
-        save.image(im = gray,file=paste0("Augmented_images/Grayscale_image",imge,".jpg"))
-      }
+  name <- getname(img)
+  imge <- getimage(img)
+  num <- 1:length(imge)
+  if(gry==TRUE){
+    gray <- lapply(imge,grayscale)
+    for (i in num){
+      save.image(im = gray[[i]],file=paste0("Augmented_images/Grayscaled_image_",name[[i]]))
     }
-    if(H&&W > 1){
-        imgrsze <- resize(imge, size_x = H, size_y = W)
-        save.image(im = imgrsze,paste0("Augmented_images/Resized_image",imge,".jpg"))
+  }
+  if(H&&W > 1){
+    imgresze <- lapply(imge,resize,size_x = H, size_y = W)
+    for (i in num){
+      save.image(im = imgresze[[i]],file=paste0("Augmented_images/Resized_image_",name[[i]]))
     }
-    if(angle>1){
-       invimg <- imrotate(im=imge,angle)
-       save.image(im = invimg,file=paste0("Augmented_images/Inverted_image",imge,".jpg"))
+  }
+  if(angle>1){
+    invimg <- lapply(imge,imrotate,angle = angle)
+    for (i in num){
+      save.image(im = invimg[[i]],file=paste0("Augmented_images/Inverted_image_",name[[i]]))
     }
+  }
 }
 
-print(augmented(args$image,H1,W1,args$grey,args$angle))
-
-list.files(pattern = "\\.jpg$", ignore.case=TRUE)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~PRINTING OUTPUT
+print(augmented(args$folder,H1,W1,args$grey,args$angle))
